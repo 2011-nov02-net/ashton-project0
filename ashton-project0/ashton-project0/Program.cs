62720c16232983c5425697aeec0217bb71dd9c41
TheAshtonaught <ashtomorgan3@gmail.com>
2020-11-10 10:29:54 -0600

    sln created

---
+++ ashton-project0/ashton-project0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ashton_project0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
